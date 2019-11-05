4b283bbc738050f8252829cb83c2f7621d869a8f
Sjoshi7 <sjoshise7@gmail.com>
2019-11-05 08:08:07 +0530

    refactored

--- test/appTest.js
+++ test/appTest.js
@@ -2,26 +2,30 @@ const assert = require('chai').assert;
 const sayLearning = require('../app.js').sayLearning;
 const addNumbers = require('../app.js').addNumbers;
 
+//Results
+sayLearningResult = sayLearning();
+addNumbersResult = addNumbers(5,5);
+
 //creating test
 describe('App',function(){
   //description of test
-  it('sayLearning should return: Learn Testing',()=>{
-    let result = sayLearning();
-    assert.equal(result,'Learn Testing');
+  describe('sayLearning()',function functionName() {
+    it('sayLearning should return: Learn Testing',()=>{
+      assert.equal(sayLearningResult,'Learn Testing');
+    });
+
+    it('sayLearning should return type string',()=>{
+      assert.typeOf(sayLearningResult,'string');
+    });
   });
 
-  it('sayLearning should return type string',()=>{
-    let result = sayLearning();
-    assert.typeOf(result,'string');
-  });
+  describe('addNumbers()',function(){
+    it('addNumbers should be above 5 for given data',function(){
+      assert.isAbove(addNumbersResult,5);
+    });
 
-  it('addNumbers should be above 5 for given data',function(){
-    let result = addNumbers(5,5);
-    assert.isAbove(result,5);
-  });
-
-  it('addNumbers should return type Number',()=>{
-    let result = addNumbers(5,5);
-    assert.typeOf(result,'number');
+    it('addNumbers should return type Number',()=>{
+      assert.typeOf(addNumbersResult,'number');
+    });
   });
 });
